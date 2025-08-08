ab368c14a51ed53184315352b8f3bc554ecf9226
Amila619 <amilasandeepa349@gmail.com>
2025-08-08 17:50:24 +0530

    - configured dotenv

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,11 @@
+import { configDotenv } from "dotenv";
 import axios from 'axios';
 
+
+const API_URL = process.env.API_URL;
+
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
