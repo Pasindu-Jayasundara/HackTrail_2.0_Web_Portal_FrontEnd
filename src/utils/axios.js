aae0f5fdb27e24af30c681150cfe6d19800e66b4
Amila619 <amilasandeepa349@gmail.com>
2025-08-08 18:27:20 +0530

    - configured environment variables

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
